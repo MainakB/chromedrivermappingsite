31b3b645594e64c4bee609da0a10e2cae625bd86
Mainak Basu <kaniam@Mainaks-MacBook-Pro.local>
2019-10-24 01:00:06 -0400

    C3

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ function App() {
 
   return (
     <div className="App">
+      <h1 className="app-header dark" style={{ "text-align": "center" }}>
+        Chrome Browser - Chromedriver Mapping
+      </h1>
       <InputChromeBrowserVersion
         handleInputChange={handleInputChange}
         inputDefaultValue={inputDefaultText}
